6516050de19a25d110b0ccba051d4528c80bcdfb
Samyuktha <samyuktha.s.tech@gmail.com>
2026-07-21 14:19:01 +0530

    added config reader file to read .properties file using getProperty method

--- src/main/java/com/eaf/config/ConfigReader.java
+++ src/main/java/com/eaf/config/ConfigReader.java
@@ -1,7 +1,23 @@
 package com.eaf.config;
 import java.util.Properties;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 
 public class ConfigReader {
 	private static Properties properties = new Properties();
-
+	static {
+		try {
+			FileInputStream fis = new FileInputStream("config.properties");
+			properties.load(fis);	
+		}
+		catch(IOException e){
+			throw new RuntimeException("Failed to load .propertied file", e);
+			
+		}
+	}
+	
+	public static String getProperty(String key) {
+		return properties.getProperty(key);
+	}
 }
